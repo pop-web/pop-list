6754bf79e5a758e8ae0a4f665ae0c89e50de081e
koga <popweb782@gmail.com>
2020-07-18 19:38:52 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex">
+    <div class="w-full bg-white">
+      <ul>
+        <li>あいうえおかきくけこ</li>
+        <li>あいうえおかきくけこ</li>
+        <li>あいうえおかきくけこ</li>
+        <li>あいうえおかきくけこ</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
