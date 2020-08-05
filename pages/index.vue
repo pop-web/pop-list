ada9c7700918fe0b1ab0c1f0955551197bad9ba6
koga <popweb782@gmail.com>
2020-08-06 00:04:58 +0900

    検索機能追加

--- pages/index.vue
+++ pages/index.vue
@@ -80,6 +80,57 @@
         </form>
         <transition-group name="todo-list" tag="ul">
           <li
+            v-for="todo in filterKeyWord"
+            :key="todo.id"
+            class="mt-3"
+            :class="{ editing: todo == editedTodo, 'todo-list-item': true }"
+          >
+            <div
+              class="w-full bg-white hover:bg-gray-200 rounded inline-flex items-center cursor-pointer"
+            >
+              <label
+                class="w-10 h-10 flex items-center justify-center cursor-pointer"
+              >
+                <input
+                  type="checkbox"
+                  class="leading-tight focus:outline-none cursor-pointer"
+                  :checked="todo.state"
+                  @click="completeTodo(todo)"
+                />
+              </label>
+              <button
+                type="button"
+                class="view-todo w-full p-4 text-left focus:outline-none focus:box-shadow"
+                @dblclick="editTodo(todo)"
+              >
+                {{ todo.comment }}
+              </button>
+              <input
+                v-model="todo.comment"
+                v-todo-focus="todo == editedTodo"
+                class="input-edit hidden w-full p-4 relative"
+                type="text"
+                @blur="doneEdit(todo)"
+                @keyup.enter="doneEdit(todo)"
+                @keyup.esc="cancelEdit(todo)"
+              />
+              <span class="p-4 text-gray-500" @click="removeTodo(todo)">
+                <svg
+                  class="fill-current w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z"
+                  />
+                </svg>
+              </span>
+            </div>
+          </li>
+        </transition-group>
+        <p>-------------</p>
+        <transition-group name="todo-list" tag="ul">
+          <li
             v-for="todo in todos"
             :key="todo.id"
             class="mt-3"
@@ -226,6 +277,18 @@ export default {
       } else {
         return false
       }
+    },
+    filterKeyWord() {
+      const searchList = []
+      for (const i in this.todos) {
+        const todo = this.todos[i]
+        // eslint-disable-next-line
+        if (todo.comment.indexOf(this.searchWord) !== -1) {
+          console.log(todo)
+          searchList.push(todo)
+        }
+      }
+      return searchList
     }
   },
   created() {
