1cb23d588357c0435aa540b93ba2e19c3274ed85
koga <popweb782@gmail.com>
2020-08-11 22:13:48 +0900

    スライドメニューの設定

--- pages/index.vue
+++ pages/index.vue
@@ -65,7 +65,7 @@
       </div>
       <aside
         class="transform top-0 right-0 w-64 bg-white fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30"
-        :class="isOpen ? 'translate-x-0' : '-translate-x-full'"
+        :class="isOpen ? 'translate-x-0' : 'translate-x-full'"
       >
         ...
       </aside>
