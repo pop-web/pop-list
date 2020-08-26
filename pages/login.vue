c5b2117156e57836d697218c75d5af688aafb095
koga <popweb782@gmail.com>
2020-08-26 23:43:31 +0900

    add login.vue

---
+++ pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex justify-center">
+    <div
+      class="flex items-center flex-col h-48 mt-20 w-2/5 p-4 bg-white shadow rounded"
+    >
+      <p class="text-center">ログイン</p>
+      <button
+        type="submit"
+        class="w-full bg-red-600 py-3 px-10 text-white rounded focus:outline-none focus:shadow-outline border"
+      >
+        Googleでログイン
+      </button>
+    </div>
+  </div>
+</template>
