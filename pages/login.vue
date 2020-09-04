43ef9602d242c1cea983b4b96c1429d288e5715a
koga <popweb782@gmail.com>
2020-09-04 22:24:29 +0900

    googleログインアイコン追加

--- pages/login.vue
+++ pages/login.vue
@@ -21,7 +21,19 @@
           type="submit"
           class="w-full bg-red-600 py-3 px-10 text-white rounded focus:outline-none focus:shadow-outline border"
         >
-          Googleでログイン
+          <div class="flex items-center justify-center">
+            <svg
+              class="fill-current h-4 w-4 mr-2"
+              viewBox="0 0 488 512"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill="currentColor"
+                d="M488 262c0 141-97 242-240 242A248 248 0 11414 73l-67 65C259 53 94 117 94 256c0 87 69 157 154 157 98 0 135-71 141-107H248v-86h236c2 13 4 25 4 42z"
+              />
+            </svg>
+            <span>Googleでログイン</span>
+          </div>
         </button>
       </form>
       <form class="w-full" @submit.prevent="login('guest')">
