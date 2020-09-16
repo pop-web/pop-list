806d7697a197253721c45f465f18c5b9fd40343e
koga <popweb782@gmail.com>
2020-09-17 00:52:57 +0900

    add persistedstate-2

--- plugins/persistedstate.js
+++ plugins/persistedstate.js
@@ -1,7 +1,16 @@
 import createPersistedState from 'vuex-persistedstate'
+import SecureLS from 'secure-ls'
+const ls = new SecureLS({ encodingType: 'aes' })
 
 export default ({ store }) => {
   createPersistedState({
-    paths: ['user'] // localStorageに保存したいstoreを入れる
+    // localStorageに保存したいstoreを入れます。
+    // 認証情報をlocalStorageに含めるため、暗号化したうえで格納する
+    paths: ['user'],
+    storage: {
+      getItem: (key) => ls.get(key),
+      setItem: (key, value) => ls.set(key, value),
+      removeItem: (key) => ls.remove(key)
+    }
   })(store)
 }
